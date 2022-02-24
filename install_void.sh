15d8ea68d8eda4cf877fc42f295acccec33fa069
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-02-24 08:25:34 +0100

    Renamed install script

---
+++ install_void.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+add_service() {
+	sudo ln -s /etc/sv/"$1" /var/service
+}
+message() {
+    echo "$1:"
+    echo "=================="
+}
+
+message "INSTALLING XTOOLS AND ENABLING NONFREE REPO"
+sudo xbps-install -Su -y xtools void-repo-nonfree
+message "INSTALLING ZSH AND SETTING IT AS DEFAULT SHELL"
+xi -y zsh
+sudo chsh -s /bin/zsh baleksa
+message "INSTALLING MESA VGA DRIVERS AND INTEL-UCODE"
+xi -y mesa-dri mesa-vaapi mesa-vdpau mesa-vulkan-intel
+xi -y intel-ucode
+message "DBUS"
+xi -y dbus && sudo ln -s /etc/sv/dbus /var/service
+message "LOGGING"
+xi -y socklog-void &&
+	add_service socklog_unix &&
+	add_service nanoklogd
+message "MAKE LINUX USE LOCALTIME FOR TIME TO BE ACCURATE WITH DUALBOOTING AND SET CHRONY"
+echo 'export HARDWARECLOCK=localtime' | sudo tee -a /etc/rc.conf
+xi -y chrony && add_service chronyd
+message "TLP FOR POWER MANAGEMENT"
+xi -y tlp && add_service tlp
+message "SEATD"
+xi -y seatd && add_service seatd && sudo usermod -a -G _seatd baleksa
+message "SWAY"
+xi -y sway swayidle swaylock
+xi -y qt5-wayland qt6-wayland
+xi -y kwayland
+message "IWD"
+xi -y iwd && add_service iwd && printf '[General]\nUseDefaultInterface=true' | sudo tee -a /etc/iwd/main.conf
+message "ZSH"
+xi zsh-autosuggestions zsh-completions zsh-syntax-highlighting zsh-history-substring-search
+message "ALACRITTY"
+xi alacritty
+message "PIPEWIRE, ALSA AND BLUETOOTH"
+xi -y pipewire libspa-bluetoots alsa-pipewire
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+xi bluez && add_service bluetoothd && sudo usermod -a -G bluetooth baleksa
+message "INSTALL OTHER PACKAGES"
+xargs -a ./manually_installed_packages_void xi -y
+message "STOW DOTFILES"
+stow */
+message "REBOOTING NOW"
+sudo reboot now
