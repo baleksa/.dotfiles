edf63db268ede2a791c29ad2d8a563090ecc7777
Aleksa Blagojevic <Aleksa Blagojevi>
2022-04-28 09:44:15 +0200

    Added switch_audio.sh: POSIX shell script that switches audio output between analog and hdmi

---
+++ scripts/.local/bin/scripts/switch_audio.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CURRENT_PROFILE=$(pactl info | grep Default\ Sink | cut -d\. -f4)
+case "$CURRENT_PROFILE" in
+    "analog-stereo" ) pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo ;;
+    "hdmi-stereo" ) pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo ;;
+esac
+
