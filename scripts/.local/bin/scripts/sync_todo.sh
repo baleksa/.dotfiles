3335168423ac32dbbed6753f26775bd7d3acc1f8
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-05-01 21:18:09 +0200

    sync_todo.sh shows --dry-run of rclone bisync and asks a user if he/she wants to run it

--- scripts/.local/bin/scripts/sync_todo.sh
+++ scripts/.local/bin/scripts/sync_todo.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-rclone bisync gdrive_void_hpi5:todo /home/baleksa/Documents/todo --exclude "todo.txt.bak" --exclude "report.txt" "$@"
+echo "Preview of what bisync will do:"
+rclone bisync --verbose --dry-run gdrive_void_hpi5:todo /home/baleksa/Documents/todo --exclude "todo.txt.bak" --exclude "report.txt"
+printf "Do the bisync? (y/n) : "
+read -r ans
+if [ "$ans" = "y" ]; then
+    rclone bisync --verbose gdrive_void_hpi5:todo /home/baleksa/Documents/todo --exclude "todo.txt.bak" --exclude "report.txt" "$@"
+    echo "Done!"
+else
+    echo "Skipped!"
+fi
