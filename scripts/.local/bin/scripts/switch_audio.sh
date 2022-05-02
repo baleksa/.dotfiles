775cf76874f2961ef41c33a683b016ea13c52d3c
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-05-02 17:38:25 +0200

    Fixed switch_audio script to properly set input source

--- scripts/.local/bin/scripts/switch_audio.sh
+++ scripts/.local/bin/scripts/switch_audio.sh
@@ -2,7 +2,10 @@
 
 CURRENT_PROFILE=$(pactl info | grep Default\ Sink | cut -d\. -f4)
 case "$CURRENT_PROFILE" in
-    "analog-stereo" ) pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo ;;
-    "hdmi-stereo" ) pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo ;;
+"analog-stereo")
+	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
+	;;
+"hdmi-stereo")
+	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
+	;;
 esac
-
