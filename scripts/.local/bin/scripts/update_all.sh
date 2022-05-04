9324696184c14cecf4f7f4bfb43600abf2ad9c01
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-05-04 13:34:41 +0200

    Removed functions which are not used any more and changed do_job to handle when job fails.

--- scripts/.local/bin/scripts/update_all.sh
+++ scripts/.local/bin/scripts/update_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# RED='\033[31m'
+RED='\033[31m'
 PURPLE='\033[35m'
 GREEN='\033[32m'
 NC='\033[0m' # No Color
@@ -10,9 +10,14 @@ do_job() {
     shift
     bye_msg="$1"
     shift
+    failed_msg="$1"
+    shift
     colored_echo "$hello_msg" "$PURPLE"
-    "$@"
-    colored_echo "$bye_msg" "$GREEN"
+    if "$@"; then
+        colored_echo "$bye_msg" "$GREEN"
+    else
+        colored_echo "$failed_msg" "$RED"
+    fi
 }
 
 colored_echo() {
@@ -21,43 +26,23 @@ colored_echo() {
 	echo "${color}${msg}${NC}"
 }
 
-update_nvim() {
-	cwd=$(pwd)
-        echo test
-	cd "$GIT_REPOS_DIR/neovim" || return
-	git checkout master 2>/dev/null
-	git pull --rebase
-	git checkout stable 2>/dev/null
-	latest_stable_version=$(git log | sed '5q;d' | sed 's/.*NVIM //')".0"
-	installed_version=$(nvim -v | sed 's/.*NVIM v//;1q')
-        printf "The latest stable version: %s\n" "$latest_stable_version"
-        printf "Installed version: %s\n" "$installed_version"
-	if [ "$latest_stable_version" = "$installed_version" ]; then
-		colored_echo "Neovim is already up to date with the latest stable version" "$GREEN"
-		return
-        fi
-	make CMAKE_BUILD_TYPE=Release
-	sudo make install
-	cd "$cwd" || exit
-}
-
 # colored_echo "Updating xbps packages:" "$PURPLE"
 # xi -Su
 # colored_echo "Done!" "$GREEN"
-do_job "Updating xbps packages:" "Done!" xi -Su
+do_job "Updating xbps packages:" "Done!" "Failed!" xi -Su
 
 # colored_echo "Updating sumneko lua language server:" "$PURPLE"
 # update_sumneko_server
 # colored_echo "Done!" "$GREEN"
-do_job "Updating sumneko lua language server:" "Done!" update_sumneko_server.sh
+do_job "Updating sumneko lua language server:" "Done!" "Failed!" update_sumneko_lua_server.sh
 
 # colored_echo "Updating neovim from github" "$PURPLE"
 # update_nvim
 # colored_echo "Done!" "$GREEN"
-do_job "Updating neovim from github" "Done!" update_nvim
+do_job "Updating neovim from github" "Done!" "Failed!" update_neovim.sh
 
 # colored_echo "Updating neovim packages via packer.nvim:" "$PURPLE"
 # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 # colored_echo "Done!" "$GREEN"
-do_job "Updating neovim packages via packer.nvim:" "\nDone!" nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+do_job "Updating neovim packages via packer.nvim:" "\nDone!" "\nFailed!" nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
