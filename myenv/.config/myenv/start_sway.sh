4106022e6c174a94e8e303ce29faf7591d35d173
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-09-29 13:43:01 +0200

    Add BEMENU_BACKED=wayland before starting sway.

--- myenv/.config/myenv/start_sway.sh
+++ myenv/.config/myenv/start_sway.sh
@@ -5,6 +5,7 @@ export XDG_CURRENT_DESKTOP=sway
 export _JAVA_AWT_WM_NONREPARENTING=1 # Fix gray window in java apps
 export QT_QPA_PLATFORM=wayland-egl
 export SWAY_SCRIPTS="$HOME/.config/sway/scripts"
+export BEMENU_BACKEND="wayland"
 # export GTK_BACKED=wayland
 # export WAYLAND_DEBUG=1 # Uncomment this line to enable more verbose logging
 # # D-Bus
