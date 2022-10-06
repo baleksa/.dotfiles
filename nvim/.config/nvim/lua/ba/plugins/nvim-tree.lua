7e59771da9c2bf9a64288eedd3c877c1b63e5da3
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-10-06 15:16:39 +0200

    Refactor.

--- nvim/.config/nvim/lua/ba/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/ba/plugins/nvim-tree.lua
@@ -1,10 +1,25 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Needed to fix nvim-tree openning on the right side
+vim.opt.splitright = true
+
 -- Open file explorer
 vim.api.nvim_set_keymap("n", "<leader>h", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 -- Open file explorer and position cursor on file thats opened
 vim.api.nvim_set_keymap("n", "<leader>ff", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
 
--- automatically close the tab/vim when nvim-tree is the last window in the tab
-vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+-- Autoclose is turned off because it doesn't work nicely with other plugins and
+-- features
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+--   pattern = "NvimTree_*",
+--   callback = function()
+--     local layout = vim.api.nvim_call_function("winlayout", {})
+--     if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+--   end
+-- })
 
 local lib = require("nvim-tree.lib")
 local view = require("nvim-tree.view")
@@ -66,6 +81,9 @@ require("nvim-tree").setup({ -- Call setup function
 	},
 	renderer = {
 		highlight_git = true,
+		indent_markers = {
+			enable = true,
+		},
 		icons = {
 			glyphs = {
 				git = {
@@ -77,6 +95,9 @@ require("nvim-tree").setup({ -- Call setup function
 					deleted = "",
 					ignored = "",
 				},
+			},
+			show = {
+				folder_arrow = false,
 			},
 		},
 	},
@@ -90,167 +111,3 @@ require("nvim-tree").setup({ -- Call setup function
 		},
 	},
 })
-
---[[ require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
-	auto_reload_on_write = true,
-	create_in_closed_folder = false,
-	disable_netrw = false,
-	hijack_cursor = false,
-	hijack_netrw = true,
-	hijack_unnamed_buffer_when_opening = false,
-	ignore_buffer_on_setup = false,
-	open_on_setup = false,
-	open_on_setup_file = false,
-	open_on_tab = false,
-	sort_by = "name",
-	update_cwd = false,
-	reload_on_bufenter = false,
-	respect_buf_cwd = false,
-	view = {
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		preserve_window_proportions = false,
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
-		mappings = {
-			custom_only = false,
-			list = {
-				-- user mappings go here
-			},
-		},
-	},
-	renderer = {
-		add_trailing = false,
-		group_empty = false,
-		highlight_git = false,
-		highlight_opened_files = "none",
-		root_folder_modifier = ":~",
-		indent_markers = {
-			enable = false,
-			icons = {
-				corner = "└ ",
-				edge = "│ ",
-				none = "  ",
-			},
-		},
-		icons = {
-			webdev_colors = true,
-			git_placement = "before",
-			padding = " ",
-			symlink_arrow = " ➛ ",
-			show = {
-				file = true,
-				folder = true,
-				folder_arrow = true,
-				git = true,
-			},
-			glyphs = {
-				default = "",
-				symlink = "",
-				folder = {
-					arrow_closed = "",
-					arrow_open = "",
-					default = "",
-					open = "",
-					empty = "",
-					empty_open = "",
-					symlink = "",
-					symlink_open = "",
-				},
-				git = {
-					unstaged = "✗",
-					staged = "✓",
-					unmerged = "",
-					renamed = "➜",
-					untracked = "★",
-					deleted = "",
-					ignored = "◌",
-				},
-			},
-		},
-		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-	},
-	hijack_directories = {
-		enable = true,
-		auto_open = true,
-	},
-	update_focused_file = {
-		enable = false,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	ignore_ft_on_setup = {},
-	system_open = {
-		cmd = "",
-		args = {},
-	},
-	diagnostics = {
-		enable = false,
-		show_on_dirs = false,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	filters = {
-		dotfiles = false,
-		custom = {},
-		exclude = {},
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 400,
-	},
-	actions = {
-		use_system_clipboard = true,
-		change_dir = {
-			enable = true,
-			global = false,
-			restrict_above_cwd = false,
-		},
-		expand_all = {
-			max_folder_discovery = 300,
-		},
-		open_file = {
-			quit_on_open = false,
-			resize_window = true,
-			window_picker = {
-				enable = true,
-				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-				exclude = {
-					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-					buftype = { "nofile", "terminal", "help" },
-				},
-			},
-		},
-		remove_file = {
-			close_window = true,
-		},
-	},
-	trash = {
-		cmd = "trash",
-		require_confirm = true,
-	},
-	live_filter = {
-		prefix = "[FILTER]: ",
-		always_show_folders = true,
-	},
-	log = {
-		enable = false,
-		truncate = false,
-		types = {
-			all = false,
-			config = false,
-			copy_paste = false,
-			diagnostics = false,
-			git = false,
-			profile = false,
-		},
-	},
-}) -- END_DEFAULT_OPTS ]]
