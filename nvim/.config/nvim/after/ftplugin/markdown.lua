280a4398d20222894f06573eb3e4dc9dbb9cbb03
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-12-11 18:02:52 +0100

    Change the name of the checking variable at the start of the ftplugin.

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
-if vim.b.did_my_after_ftplugin then
+if vim.b.did_after_ftplugin then
 	return
 end
-vim.b.did_my_after_ftplugin = true
+vim.b.did_after_ftplugin = true
 
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "sr,en_us"
