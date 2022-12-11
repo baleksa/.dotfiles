f04c68610b48d471bad3febbac79514a5a2cc763
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-12-11 18:07:21 +0100

    Factor functions for changing colorsheme. Use oxocarbon colroscheme.

--- nvim/.config/nvim/lua/ba/colorscheme.lua
+++ nvim/.config/nvim/lua/ba/colorscheme.lua
@@ -1,13 +1,29 @@
-require("onedark").setup({ -- Set colorcheme options
-	style = "deep",
-	code_style = {
-		comments = "none",
-	},
-})
+local setOxocarbon = function()
+	vim.opt.background = "dark" -- set this to dark or light
+	vim.cmd.colorscheme("oxocarbon")
+end
 
 local setOneDark = function()
+	require("onedark").setup({ -- Set colorcheme options
+		style = "deep",
+		code_style = {
+			comments = "none",
+		},
+	})
 	require("onedark").load()
 	require("lualine").setup({ options = { theme = "onedark" } })
+	-- Fix color of one rainbow parentheses which wasn't blending nicely with
+	-- OneDark colorscheme
+	vim.cmd("hi rainbowcol1 guifg=#ffffff")
+
+	-- Set color for filenames of unstaged files in nvim-tree.lua to the current
+	-- theme's red color
+	local setNvimTreeGitDirtyHighlighColor = function()
+		local red = require("onedark.colors").red
+		vim.cmd("highlight NvimTreeGitDirty guifg=" .. red)
+	end
+	setNvimTreeGitDirtyHighlighColor()
+	vim.api.nvim_create_autocmd("ColorScheme", { pattern = "onedark", callback = setNvimTreeGitDirtyHighlighColor })
 end
 
 local setMonokai = function()
@@ -21,7 +37,7 @@ local setMoonlight = function()
 end
 
 local theme_index = 1
-local setThemeFunctions = { setOneDark, setMoonlight, setMonokai }
+local setThemeFunctions = { setOxocarbon ,setOneDark, setMoonlight, setMonokai }
 setThemeFunctions[theme_index]()
 ChangeTheme = function()
 	theme_index = theme_index + 1
@@ -32,17 +48,4 @@ ChangeTheme = function()
 end
 vim.api.nvim_set_keymap("n", "<leader>tc", "<CMD>lua ChangeTheme()<CR>", { noremap = true, silent = true })
 
--- Fix color of one rainbow parentheses which wasn't blending nicely with
--- OneDark colorscheme
-vim.cmd("hi rainbowcol1 guifg=#ffffff")
-
 vim.cmd("highlight WinSeparator guibg=None") -- Crisp whiteline between windows
-
--- Set color for filenames of unstaged files in nvim-tree.lua to the current
--- theme's red color
-local setNvimTreeGitDirtyHighlighColor = function()
-	local red = require("onedark.colors").red
-	vim.cmd("highlight NvimTreeGitDirty guifg=" .. red)
-end
-setNvimTreeGitDirtyHighlighColor()
-vim.api.nvim_create_autocmd("ColorScheme", { pattern = "onedark", callback = setNvimTreeGitDirtyHighlighColor })
