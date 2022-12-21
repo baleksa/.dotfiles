da576f6c29a7326dbf29707d52d121b9a2055735
Aleksa Balgojević <skr.aleksa@gmail.com>
2022-12-21 20:11:59 +0100

    Refactor file explorer configuration.

---
+++ nvim/.config/nvim/lua/ba/plugins/file_exp.lua
@@ -0,0 +1,12 @@
+local file_exp = "nvim-tree" -- nvim_tree or netrw
+
+local file_explorers = {
+	nvim_tree = function()
+		require("ba.plugins.nvim-tree")
+	end,
+	netrw = function()
+		require("ba.plugins.netrw")
+	end
+}
+
+file_explorers[file_exp]()
