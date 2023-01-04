b3ed02040f69ff0649e1ea9ab9ff1bb516a047d8
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-01-04 14:48:50 +0100

    Refactor.

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,8 @@
-if vim.b.did_after_ftplugin then
+if vim.b.did_my_after_ftplugin then
 	return
 end
-vim.b.did_after_ftplugin = true
+vim.b.did_my_after_ftplugin = true
 
 vim.opt_local.spell = true
-vim.opt_local.spelllang = "sr,en_us"
+vim.opt_local.spelllang = { "sr", "en_us" }
 
