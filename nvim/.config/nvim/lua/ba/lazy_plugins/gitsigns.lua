f02c8c1111546c5c6178c76cee424c47faa5d129
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-01-16 16:17:33 +0100

    Use mid block unicode character because it looks better in new statuscolumn.

--- nvim/.config/nvim/lua/ba/lazy_plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/gitsigns.lua
@@ -10,13 +10,13 @@ function M.config()
 		signs = {
 			add = {
 				hl = "GitSignsAdd",
-				text = "▎",
+				text = "🮇",
 				numhl = "GitSignsAddNr",
 				linehl = "GitSignsAddLn",
 			},
 			change = {
 				hl = "GitSignsChange",
-				text = "▎",
+				text = "🮇",
 				numhl = "GitSignsChangeNr",
 				linehl = "GitSignsChangeLn",
 			},
@@ -34,7 +34,7 @@ function M.config()
 			},
 			changedelete = {
 				hl = "GitSignsChange",
-				text = "▎",
+				text = "🮇",
 				numhl = "GitSignsChangeNr",
 				linehl = "GitSignsChangeLn",
 			},
@@ -45,6 +45,56 @@ function M.config()
 				linehl = "GitSignsAddLn",
 			},
 		},
+		on_attach = function(bufnr)
+			local gs = package.loaded.gitsigns
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			-- Navigation
+			map("n", "]g", function()
+				if vim.wo.diff then
+					return "]c"
+				end
+				vim.schedule(function()
+					gs.next_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true })
+
+			map("n", "[g", function()
+				if vim.wo.diff then
+					return "[c"
+				end
+				vim.schedule(function()
+					gs.prev_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true })
+
+			-- Actions
+			-- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+			-- map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+			-- map("n", "<leader>hS", gs.stage_buffer)
+			-- map("n", "<leader>hu", gs.undo_stage_hunk)
+			-- map("n", "<leader>hR", gs.reset_buffer)
+			-- map("n", "<leader>hp", gs.preview_hunk)
+			-- map("n", "<leader>hb", function()
+			-- 	gs.blame_line({ full = true })
+			-- end)
+			-- map("n", "<leader>tb", gs.toggle_current_line_blame)
+			-- map("n", "<leader>hd", gs.diffthis)
+			-- map("n", "<leader>hD", function()
+			-- 	gs.diffthis("~")
+			-- end)
+			-- map("n", "<leader>td", gs.toggle_deleted)
+			--
+			-- -- Text object
+			-- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+		end,
 	})
 end
 
