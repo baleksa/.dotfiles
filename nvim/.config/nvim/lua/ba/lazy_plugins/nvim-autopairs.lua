04c492fe9037a9a4943cc2cce8ff4c93e23cfe77
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-03-04 11:57:00 +0100

    Change config: update config.

--- nvim/.config/nvim/lua/ba/lazy_plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/nvim-autopairs.lua
@@ -13,19 +13,18 @@ function M.config()
 		fast_wrap = {
 			map = "<M-e>",
 			chars = { "{", "[", "(", '"', "'" },
-			pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-			offset = 0, -- Offset from pattern match
+			pattern = [=[[%'%"%>%]%)%}%,]]=],
 			end_key = "$",
 			keys = "qwertyuiopzxcvbnmasdfghjkl",
 			check_comma = true,
-			highlight = "PmenuSel",
-			highlight_grey = "LineNr",
+			highlight = "Search",
+			highlight_grey = "Comment",
 		},
 	})
 	-- If you want insert `(` after select function or method item
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 	local cmp = require("cmp")
-	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
