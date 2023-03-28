574035fce3fab3e3e800fb07de123615a3d3b198
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-03-28 16:35:48 +0200

    nvim: treesitter.nvim: Add playground.

--- nvim/.config/nvim/lua/ba/lazy_plugins/treesitter.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/treesitter.lua
@@ -5,12 +5,31 @@ local M = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"JoosepAlviste/nvim-ts-context-commentstring",
 		"HiPhish/nvim-ts-rainbow2",
+		"nvim-treesitter/playground",
 	},
 }
 
 function M.config()
 	require("nvim-treesitter.configs").setup({
 		auto_install = true,
+		playground = {
+			enable = true,
+			disable = {},
+			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+			persist_queries = false, -- Whether the query persists across vim sessions
+			keybindings = {
+				toggle_query_editor = 'o',
+				toggle_hl_groups = 'i',
+				toggle_injected_languages = 't',
+				toggle_anonymous_nodes = 'a',
+				toggle_language_display = 'I',
+				focus_language = 'f',
+				unfocus_language = 'F',
+				update = 'R',
+				goto_node = '<cr>',
+				show_help = '?',
+			},
+		},
 		highlight = {
 			enable = true,
 			-- disable = { "help", },
