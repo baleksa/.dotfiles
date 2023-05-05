790dfe6512f5173747b37488bc93d82a72f1047a
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-05-05 07:42:28 +0200

    zsh: Set aliases for reading $HISTFILE.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,3 +1,9 @@
-#!/bin/zsh
+# Zsh aliases
 
-. "$MYRC_DIR/aliases"
+. "$MYRC_DIR/aliases" # Source my universal aliases
+
+# If you want a command from an other shell instance first write local history
+# to $HISTFILE from that shell, then read history from current shell.
+# Writing first is not needed if INC_APPEND_HISTORY is set
+alias shi="fc -AI" # Write current shell's history to $HISTFILE
+alias rhi="fc -RI" # Read all history from $HISTFILE to current shell's
