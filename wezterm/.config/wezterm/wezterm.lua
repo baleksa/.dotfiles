a3c153916159b71b4142c79e3fcbe708dcdd2786
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-05-05 07:41:56 +0200

    wezterm: Use day/night colorscheme changing.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,6 +14,25 @@ local select_and_open_url = act.QuickSelectArgs({
 		wezterm.open_with(url)
 	end),
 })
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	-- If something isn't right use Light as a default fallback.
+	return "Light"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Everforest Dark (Hard)"
+	else
+		return "Everforest Light (Hard)"
+	end
+end
+
 ---
 --- Final configuration table
 ---
@@ -35,7 +54,7 @@ return {
 	font_size = 12,
 	term = "wezterm",
 	color_scheme_dirs = { "~/.config/wezterm/colors" },
-	color_scheme = "Everforest Light (Hard)",
+	color_scheme = scheme_for_appearance(get_appearance()),
 	check_for_updates = false,
 	-- show_update_window = false,
 	hide_tab_bar_if_only_one_tab = true,
