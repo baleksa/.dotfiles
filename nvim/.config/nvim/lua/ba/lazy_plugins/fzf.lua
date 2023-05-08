7f091f6809782f6f8ddd9be2fb73630f65c40f7b
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-05-08 18:05:26 +0200

    nvim: fzf-lua: Use fzf-lua intead of telescope.

--- nvim/.config/nvim/lua/ba/lazy_plugins/fzf.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/fzf.lua
@@ -1,8 +1,38 @@
 local M = {
-	"ibhagwan/fzf-lua"
+  "ibhagwan/fzf-lua",
 }
 
 function M.config()
+  vim.keymap.set("n", "<leader><space>", function()
+    require("fzf-lua").buffers()
+  end, { silent = true, desc = "Search opened buffers" })
+  vim.keymap.set("n", "<leader>sf", function()
+    require("fzf-lua").files()
+  end, { silent = true, desc = "Search files in cwd" })
+  vim.keymap.set("n", "<leader>sb", function()
+    require("fzf-lua").grep_curbuf()
+  end, { silent = true, desc = "Fuzzy search current buffer" })
+  vim.keymap.set("n", "<leader>sh", function()
+    require("fzf-lua").help_tags()
+  end, { silent = true, desc = "Search help documentation" })
+  vim.keymap.set("n", "<leader>st", function()
+    require("fzf-lua").tags()
+  end, { silent = true, desc = "Search tags" })
+  vim.keymap.set("n", "<leader>sd", function()
+    require("fzf-lua").grep_cword()
+  end, { silent = true, desc = "Search files in cwd for word under cursor" })
+  vim.keymap.set("n", "<leader>sg", function()
+    require("fzf-lua").grep_project()
+  end, { silent = true, desc = "Live grep" })
+  vim.keymap.set("n", "<leader>so", function()
+    require("fzf-lua").btags()
+  end, { silent = true, desc = "Search current buffer tags" })
+  vim.keymap.set("n", "<leader>?", function()
+    require("fzf-lua").oldfiles()
+  end, { silent = true, desc = "Search previously opened files" })
+  -- vim.keymap.set("n", "<leader>fcc", function()
+  --   require("fzf-lua").colorschemes()
+  -- end, { silent = true, desc = "Pick a colorscheme" })
 end
 
 return M
