683c74d88292a90b1828f36368d7729a45aee002
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-05-13 07:44:15 +0200

    nvim: fzf-lua: Fix: Override global mappings.

--- nvim/.config/nvim/lua/ba/lazy_plugins/fzf.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/fzf.lua
@@ -6,8 +6,8 @@ return {
         keymap = {
           builtin = {},
           fzf = {
-            ["ctrl-k"] = "up",
-            ["ctrl-j"] = "down",
+            -- ["ctrl-k"] = "up",
+            -- ["ctrl-j"] = "down",
           },
         },
       })
@@ -15,10 +15,10 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "fzf",
         callback = function()
-          vim.keymap.del({ "n", "t" }, "<c-k>")
-          vim.keymap.del({ "n", "t" }, "<c-j>")
-          vim.keymap.del({ "n", "t" }, "<c-h>")
-          vim.keymap.del({ "n", "t" }, "<c-l>")
+          vim.keymap.set({ "n", "t" }, "<c-k>", "<c-k>", {buffer = true})
+          vim.keymap.set({ "n", "t" }, "<c-j>", "<c-j>", {buffer = true})
+          vim.keymap.set({ "n", "t" }, "<c-h>", "<c-h>", {buffer = true})
+          vim.keymap.set({ "n", "t" }, "<c-l>", "<c-l>", {buffer = true})
         end,
         group = vim.api.nvim_create_augroup("FzfMap", { clear = true }),
       })
