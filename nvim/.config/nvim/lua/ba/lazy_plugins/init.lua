f9781686cf97c0636a093afe1f424e69ecbf61d1
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-10-17 15:15:52 +0200

    nvim: nvim-ufo: Make nvim-ufo work nice with lazy.nvim.

--- nvim/.config/nvim/lua/ba/lazy_plugins/init.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/init.lua
@@ -176,18 +176,42 @@ return {
   { "timmyjose-projects/lox.vim" },
   {
     "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = "BufRead",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+      },
+      {
+        "K",
+        function()
+          local winid = require("ufo").peekFoldedLinesUnderCursor()
+          if not winid then
+            vim.lsp.buf.hover()
+          end
+        end,
+      },
+    },
     config = function()
       vim.o.foldcolumn = "1" -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
 
-      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
       require("ufo").setup({
-        -- 					function(bufnr, filetype, buftype)
-        provider_selector = function(_, _, _)
+        provider_selector = function(bufnr, filetype, buftype)
           return { "treesitter", "indent" }
         end,
       })
