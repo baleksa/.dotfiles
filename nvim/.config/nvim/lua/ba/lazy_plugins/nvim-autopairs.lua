904b5c98d580a79e8821eb971256611db5a3593f
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-10-17 15:16:15 +0200

    nvim: nvim-autopairs: Update config to work with updated lazy.nvim and nvim.

--- nvim/.config/nvim/lua/ba/lazy_plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/nvim-autopairs.lua
@@ -1,30 +1,30 @@
-local M = { "windwp/nvim-autopairs" } -- auto insert } after {<CR>,
-
-function M.config()
-	local npairs = require("nvim-autopairs")
-	npairs.setup({
-		check_ts = true,
-		ts_config = {
-			lua = { "string", "source" },
-			javascript = { "string", "template_string" },
-			java = false,
-		},
-		disable_filetype = { "TelescopePrompt", "spectre_panel" },
-		fast_wrap = {
-			map = "<M-e>",
-			chars = { "{", "[", "(", '"', "'" },
-			pattern = [=[[%'%"%>%]%)%}%,]]=],
-			end_key = "$",
-			keys = "qwertyuiopzxcvbnmasdfghjkl",
-			check_comma = true,
-			highlight = "Search",
-			highlight_grey = "Comment",
-		},
-	})
-	-- If you want insert `(` after select function or method item
-	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-	local cmp = require("cmp")
-	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-end
-
-return M
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = [=[[%'%"%>%]%)%}%,]]=],
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "Search",
+      highlight_grey = "Comment",
+    },
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
