8e28aec82a402fd74f6f1225c2b324c3755ec1d7
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-12-01 14:48:16 +0100

    nvim: nvim-lint: Find conf file for selene and set it in cmdline arg.

--- nvim/.config/nvim/lua/ba/lazy_plugins/nvim_lint.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/nvim_lint.lua
@@ -1,13 +1,33 @@
 return {
   "mfussenegger/nvim-lint",
   config = function()
+    local selene = require("lint").linters.selene
+    selene.args = {
+      "--config",
+      function()
+        local conf = vim.fs.find(
+          { "selene.toml" },
+          { type = "file", upward = true, path = vim.api.nvim_buf_get_name(0) }
+        )[1]
+        if conf == nil then
+          conf = vim.fn.expand("~/.config/selene/selene.toml")[1]
+        end
+        return conf
+      end,
+      "--display-style",
+      "json",
+      "-",
+    }
     require("lint").linters_by_ft = {
       lua = { "selene" },
     }
-    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "BufEnter" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
+    vim.api.nvim_create_autocmd(
+      { "BufWritePost", "InsertLeave", "TextChanged" },
+      {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      }
+    )
   end,
 }
