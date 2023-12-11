4fe389e186ed12469ea6fa8a99313f43da12926c
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-12-11 10:13:15 +0100

    nvim: Add nvim-spider plugin.

--- nvim/.config/nvim/lua/ba/lazy_plugins/init.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/init.lua
@@ -341,4 +341,35 @@ return {
       end, {})
     end,
   },
+  {
+    "chrisgrieser/nvim-spider",
+    opts = {},
+    config = function(_, opts)
+      require("spider").setup(opts)
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        { desc = "Spider-w" }
+      )
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        { desc = "Spider-e" }
+      )
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        { desc = "Spider-b" }
+      )
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "ge",
+        "<cmd>lua require('spider').motion('ge')<CR>",
+        { desc = "Spider-ge" }
+      )
+    end,
+  },
 }
