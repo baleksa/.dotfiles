c4ff8068ac0ef48b216729b650f2bcff2fb1f766
Aleksa Balgojević <skr.aleksa@gmail.com>
2023-12-26 10:46:56 +0100

    nvim: rainbow-delim: Set strategy in respect to file size.

--- nvim/.config/nvim/lua/ba/lazy_plugins/treesitter.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/treesitter.lua
@@ -155,8 +155,8 @@ return {
             local line_count = vim.api.nvim_buf_line_count(bufnr)
             if line_count > 2000 then
               return nil
-              -- elseif line_count > 1000 then
-              --   return rainbow.strategy["global"]
+            elseif line_count > 1000 then
+              return rainbow.strategy["global"]
             end
             return rainbow.strategy["local"]
           end,
