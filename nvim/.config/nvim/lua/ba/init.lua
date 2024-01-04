95b4d2d1a77ecea5348ffc4701461cc9939fda68
Aleksa Balgojević <skr.aleksa@gmail.com>
2024-01-04 17:44:21 +0100

    nvim: Use oil as the file browser.

--- nvim/.config/nvim/lua/ba/init.lua
+++ nvim/.config/nvim/lua/ba/init.lua
@@ -2,13 +2,15 @@ require("ba.vim")
 
 vim.g.status_line = "cokeline"
 vim.g.too_large_file_lnum = 2000
-vim.g.file_explorer = "mini"
+vim.g.file_explorer = "oil"
 
 require("ba.lazy")
 require("ba.map")
+
 if vim.g.file_explorer == "netrw" then
   require("ba.netrw").setup()
 end
+
 -- nfnl compiled lua
 -- I can't find better solution to mix lua and fennel
 require("ba.lua")
