8dd16d48c9f5de14f3898d521b656dc654e0f4c4
Aleksa Balgojević <skr.aleksa@gmail.com>
2024-01-04 17:41:44 +0100

    nvim: Small changes to lisp plugins conf.

--- nvim/.config/nvim/lua/ba/lazy_plugins/lisp.lua
+++ nvim/.config/nvim/lua/ba/lazy_plugins/lisp.lua
@@ -1,8 +1,9 @@
 local M = {
   { "Olical/nfnl", ft = "fennel" },
+  { "Olical/aniseed" },
   {
     "Olical/conjure",
-    ft = { "clojure", "fennel", "python" }, -- etc
+    ft = { "clojure", "fennel", "python", "lua" },
     -- [Optional] cmp-conjure for cmp
     dependencies = {
       {
@@ -23,7 +24,7 @@ local M = {
       },
     },
     opts = {},
-    config = function(_, opts)
+    config = function(_, _)
       require("conjure.main").main()
       require("conjure.mapping")["on-filetype"]()
     end,
@@ -35,12 +36,8 @@ local M = {
       vim.g["conjure#mapping#doc_word"] = false
       -- Rebind it from K to <prefix>gk
       vim.g["conjure#mapping#doc_word"] = "gk"
+      vim.g["conjure#extract#tree_sitter#enabled"] = true
     end,
-  },
-  {
-    "julienvincent/nvim-paredit",
-    ft = { "closure", "fennel" },
-    opts = {},
   },
   {
     "julienvincent/nvim-paredit-fennel",
@@ -48,5 +45,78 @@ local M = {
     ft = { "fennel" },
     opts = {},
   },
+  -- {
+  --   "julienvincent/nvim-paredit",
+  --   ft = { "clojure", "fennel" },
+  --   opts = {},
+  -- },
+  {
+    "PaterJason/nvim-treesitter-sexp",
+    ft = { "clojure", "fennel", "janet", "query" },
+    opts = {
+      -- Enable/disable
+      enabled = true,
+      -- Move cursor when applying commands
+      set_cursor = true,
+      -- Set to false to disable all keymaps
+      keymaps = {
+        -- Set to false to disable keymap type
+        commands = {
+          -- Set to false to disable individual keymaps
+          swap_prev_elem = "<e",
+          swap_next_elem = ">e",
+          swap_prev_form = "<f",
+          swap_next_form = ">f",
+          promote_elem = "<LocalLeader>O",
+          promote_form = "<LocalLeader>o",
+          splice = "<LocalLeader>@",
+          slurp_left = "<(",
+          slurp_right = ">)",
+          barf_left = ">(",
+          barf_right = "<)",
+          insert_head = "<I",
+          insert_tail = ">I",
+        },
+        motions = {
+          form_start = "(",
+          form_end = ")",
+          prev_elem = "[e",
+          next_elem = "]e",
+          prev_elem_end = "[E",
+          next_elem_end = "]E",
+          prev_top_level = "[[",
+          next_top_level = "]]",
+        },
+        textobjects = {
+          inner_elem = "ie",
+          outer_elem = "ae",
+          inner_form = "if",
+          outer_form = "af",
+          inner_top_level = "iF",
+          outer_top_level = "aF",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("treesitter-sexp").setup(opts)
+    end,
+  },
+  {
+    "eraserhd/parinfer-rust",
+    ft = { "clojure", "fennel", "lisp", "scheme" },
+    -- opts = {},
+    -- config = function(plugin, _)
+    --   -- vim.opt.rtp:append(plugin.dir .. "target/release")
+    -- end,
+    build = "cargo build --release",
+  },
+  -- {
+  --   "harrygallagher4/nvim-parinfer-rust",
+  --   ft = { "clojure" },
+  --   opts = {},
+  --   config = function(_, opts)
+  --     require("parinfer").setup(opts)
+  --   end,
+  -- },
 }
 return M
